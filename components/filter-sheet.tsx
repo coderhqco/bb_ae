fe490273269670fdf9f78e4646d4781742c63e1e
Zaman Ehsani <zamanehsani@gmail.com>
2025-06-11 03:14:48 +0400

    refactoring

--- components/filter-sheet.tsx
+++ components/filter-sheet.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
 import {
   Sheet,
   SheetClose,
@@ -29,14 +29,14 @@ export function FilterSheet() {
           </SheetDescription>
         </SheetHeader>
         <div className="grid flex-1 auto-rows-min gap-6 px-4">
-          <div className="grid gap-3">
+          {/* <div className="grid gap-3">
             <Label htmlFor="sheet-demo-name">Name</Label>
             <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
           </div>
           <div className="grid gap-3">
             <Label htmlFor="sheet-demo-username">Username</Label>
             <Input id="sheet-demo-username" defaultValue="@peduarte" />
-          </div>
+          </div> */}
         </div>
         <SheetFooter>
           <Button type="submit">Save changes</Button>
